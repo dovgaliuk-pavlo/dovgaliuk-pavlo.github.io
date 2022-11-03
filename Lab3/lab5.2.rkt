0414962bfdd2fa80592df57e950846ae4399b5d4
Gratky <dovgaliuk.pavlo@knu.ua>
2022-11-03 19:03:27 +0200

    Lab 3. COMPLETE.

--- Lab3/lab5.2.rkt
+++ Lab3/lab5.2.rkt
@@ -11,21 +11,31 @@
 (display Simp)
 
 
-;функція циклу
-(define (loop min max func accur)
-  (if (<= max min)
-    (func min)
-    (loop (+ min accur) max func accur)))
 
-; Трапеції           
-(define sum 0)              
-(define (trap a b n func)
-        (loop a b (lambda (x) (set! sum (+ sum (func x)))) (/ (- b a) n))
-        (* (+ ( / (+ (func a) (func b)) 2) sum) (/ (- b a) n)))  
+; Трапеції
+(define (set_h a b n) (/ (- b a) n))
+(define (set_S a b func) (+ (func a) (func b)))
+
+
+(define eps 0.001)
+
+(define (loop h S func i n)
+  (if (= i (- n 1))
+      (* S (/ h 2))
+      (loop h (+ S (* 2 (func (* h i)))) func (+ i 1) n)))
+
+(define (trap a b n S1 func)
+  (let ((S (loop (set_h a b n) (set_S a b func) func 1 n)))
+    (if (<= (abs (- S S1)) eps)
+        (trap a b (* n 2) S func)
+        S
+     )
+  )
+)
 
 (newline)
 (display "Формула трапецій (n=1000) = ")
-(define Trap (trap 0 1 1000 (lambda(x)
+(define Trap (trap 0 1 1000 0 (lambda(x)
                            (sin (+ x (+ 1 (expt x 2)))))))
 (display Trap)
 ; difference
