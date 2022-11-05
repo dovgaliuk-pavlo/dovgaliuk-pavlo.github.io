21a7dfbfe74d4f267365a51f9fecf18369ba6792
Gratky <dovgaliuk.pavlo@knu.ua>
2022-11-05 18:47:34 +0200

    Lab4. Completed.

--- Lab4/lab5.1.rkt
+++ Lab4/lab5.1.rkt
@@ -1,42 +1,67 @@
+(#%require racket/list)
+
 (display "Довгалюк Павло ІПЗ-41. Лаб 4. Варіант 5. Частина 1.\n")
 
-(define (display_formated . m_part)
-  (for-each display m_part))
+(define x 2)
+(define n 5)
 
-(define a 5)
-(define b 10)
-(define acc 0.0001)
-(define root 0)
+; creation
+(define (getListPowers x i n)
+  (cond ((= i n) '())
+        ((< i n) (cons (expt x i) (getListPowers x (+ i 1) n)))))
 
-(define (average a b) (/ (+ a b) 2))
-(define (isAccurate x) (< (abs x) acc))
+(define (createListPowers x n)
+  (let ((i 1) (n (+ n 1)))
+    (getListPowers x i n)))
 
-(define (func x) (- (+ 5 (cos x)) x))
-(define (derFunc x) (- -1 (sin x)))
+; task a
+(define (returnByStep lst i n step)
+  (cond ((> i ( - n 1)) '())
+        ((= i ( - n 1)) (list (list-ref lst i)))
+        ((< i ( - n 1)) (cons (list-ref lst i) (returnByStep lst (+ i step) n step)))))
+
+(define (getOddElements lst n position)
+  (returnByStep lst 0 n position))
+
+(define (getEvenElements lst n position)
+  (returnByStep lst 1 n position))
+
+; task b
+(define (reverseByPosition lst i)
+  (cond ((<= i 0) (reverse lst))
+        ((> i (length lst)) (reverse lst))
+        (else (append
+             (reverse (member (list-ref lst (- i 1)) (reverse lst)))
+             (reverse (member (list-ref lst i) lst))))
+        )
+  )
+
+; task c
+(define (lstSum lst)
+    (if (null? lst)
+        0
+        (+ (car lst) (lstSum (cdr lst)))))
 
 
-(define (bisec a b)
-  (let ((divideStep (average a b)))
-    (if (isAccurate (func divideStep))
-        (set! root divideStep)
-        (cond ((or (and (negative? (func a)) (positive? (func divideStep)))
-                  (and (positive? (func a)) (negative? (func divideStep)))) (bisec a divideStep))
-              ((or(and (negative? (func divideStep)) (positive? (func b)))
-                  (and (positive? (func divideStep)) (negative? (func b)))) (bisec divideStep b))))))
-
-(define x1 0)
-(define (newton x0)
-        (set! x1 (- x0 (/ (func x0) (derFunc x0))))
-            (if (isAccurate(- x1 x0))
-            (set! root x0)
-            (newton x1)))
-  
-(display "Bisec method: ")
-(bisec a b)
-root
+; output
+(display "List filled with powers of 2: ")
+(define PowersO2 (createListPowers x n))
+PowersO2
 (newline)
 
-(display "Newton method: ")
-(newton a)
-root
+(display "a) List with second elements deleted: ")
+(getOddElements PowersO2 n 2)
+(newline)
+
+(display "b) Inverted list by index (2): ")
+(reverseByPosition PowersO2 2)
+(newline)
+
+
+(display "c) Sum even elements of list ")
+(define lst (getOddElements PowersO2 n 2))
+(display lst)
+(display ": ")
+(lstSum lst)
+(newline)
 
--- Lab4/lab5.2.rkt
+++ Lab4/lab5.2.rkt
@@ -1,44 +1,66 @@
+(#%require scheme/base)
+(#%require scheme/list)
+
 (display "Довгалюк Павло ІПЗ-41. Лаб 4. Варіант 5. Частина 2.\n")
 
-
-; Сімпсона  
-(define (Sipmsons x0 x02h f)
-    (let ((h (/ ( - x02h x0) 2)))
-    (* (/ h 3) ( + (+ (f x0) (* 4 (f (+ x0 h)))) (f (+ x0 (* 2 h)))))))
-
-(define Simp (Sipmsons 0 1 (lambda(x)
-                           (sin (+ x (+ 1 (expt x 2)))))))
-(display Simp)
+(define buyers (list 5 3 1 2 4))
+(define serviceTime (list 21 18 25 16 48))
 
 
+(define (minLst lst)
+  (if (= (length lst) 1)
+      (car lst)
+      (min (car lst) (minLst (cdr lst)))
+      ))
 
-; Трапеції
-(define (set_h a b n) (/ (- b a) n))
-(define (set_S a b func) (+ (func a) (func b)))
 
-
-(define eps 0.001)
-
-(define (loop h S func i n)
-  (if (= i (- n 1))
-      (* S (/ h 2))
-      (loop h (+ S (* 2 (func (* h i)))) func (+ i 1) n)))
-
-(define (trap a b n S1 func)
-  (let ((S (loop (set_h a b n) (set_S a b func) func 1 n)))
-    (if (<= (abs (- S S1)) eps)
-        (trap a b (* n 2) S func)
-        S
-     )
+(define (countInLineTime timeLst counter)
+  (if (null? timeLst)
+      '()
+      (cons (+ counter (car timeLst))
+            (countInLineTime (cdr timeLst) (+ counter (car timeLst))))
+      )
   )
-)
 
-(newline)
-(display "Формула трапецій (n=1000) = ")
-(define Trap (trap 0 1 1000 0 (lambda(x)
-                           (sin (+ x (+ 1 (expt x 2)))))))
-(display Trap)
-; difference
-(newline)
-(display "Різниця між методами = ")
-(display (- Trap Simp))
+(define (countInLineTime timeLst counter)
+  (if (null? timeLst)
+      '()
+      (cons (+ counter (car timeLst))
+            (countInLineTime (cdr timeLst) (+ counter (car timeLst))))
+      )
+  )
+
+(define (lstIndexOf lst x)
+  (- (length lst) (length (member x lst))))
+
+(define (showLine personId inLineTime)
+  (sleep 1) ;(sleep inLineTime)
+  (display "Person(id): ")
+  (display personId)
+  (display ";  Time in line: ")
+  (display inLineTime)
+  (newline))
+
+(define (lineProcess buyers buyersInLineTime)
+  (showLine (car buyers) (car buyersInLineTime))
+  (if (= (length buyers) 1)
+      (display "End of queue")
+      (lineProcess (cdr buyers) (cdr buyersInLineTime))
+      )
+  )
+
+(define minServiceTime (minLst serviceTime))
+(define indexOfMinServiceTime (lstIndexOf serviceTime minServiceTime))
+(define minServiceTimeBuyerId (list-ref buyers indexOfMinServiceTime))
+(define buyersInLineTime (countInLineTime serviceTime 0))
+
+
+(display "Buyers(id): ")
+buyers
+(display "Time of service(sec): ")
+serviceTime
+(display "A person with the shortest service time(id): ")
+minServiceTimeBuyerId
+
+(lineProcess buyers buyersInLineTime)
+
