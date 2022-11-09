7537906a56ea770a731b4a96b45d82d392e31d65
Gratky <dovgaliuk.pavlo@knu.ua>
2022-11-09 17:10:39 +0200

    Lab6. Task done. TODO: Create lab5, lab6 reports.

--- Lab6/lab5.1.rkt
+++ Lab6/lab5.1.rkt
@@ -1,41 +1,4 @@
-(display "Довгалюк Павло ІПЗ-41. Лаб 5. Варіант 5. Частина 1.\n")
-
-;===========сума двох векторів===============
-(define add-vectors
-  (lambda (vec-1 vec-2)
-    (let* ((len (vector-length vec-1))  ; The vectors shouldhave the same length.
-           (result (make-vector len)))
-      (do ((index 0 (+ index 1)))
-        ((= index len) result)
-        (vector-set! result index
-                     (+ (vector-ref vec-1 index)
-                        (vector-ref vec-2 index)))))))
-
-;===========сума елементів вектора=================
-(define vector-sum
-  (lambda (vec)
-    (let ((len (vector-length vec))
-          (result 0))        
-      (do ((index 0 (+ index 1)))
-          ((= index len) result)
-        (set! result (+ result (vector-ref vec index)))))))
-
-;=================об'єднання векторів===================
-(define vector-append
-  (lambda vecs
-    (let* ((len (apply + (map vector-length vecs)))
-           (result (make-vector len)))
-      (let loop ((result-index 0)
-                 (source-index 0)
-                 (rest-of-vecs vecs))
-        (cond ((null? rest-of-vecs) result)
-              ((= source-index (vector-length (car rest-of-vecs)))
-               (loop result-index 0 (cdr rest-of-vecs)))
-              (else
-               (vector-set! result result-index
-                            (vector-ref (car rest-of-vecs) source-index))
-               (loop (+ result-index 1) (+ source-index 1) rest-of-vecs)))))))
-
+(display "Довгалюк Павло ІПЗ-41. Лаб 6. Варіант 5. Частина 1.\n")
 
 ;==================мінімальний елемент вектору==================
 (define (vector-min vec)
@@ -60,8 +23,6 @@
   )
 
 ;=================найбільший серед від’ємних та найменший серед додатних елементів вектору===================
-
-
 (define (minimax vec)
   (let* ((len (vector-length vec))
         (result (vector (vector-min vec) (vector-max vec))))
--- Lab6/lab5.2.rkt
+++ Lab6/lab5.2.rkt
@@ -1,52 +1,73 @@
-(display "Довгалюк Павло ІПЗ-41. Лаб 5. Варіант 5. Частина 2.\n")
+(display "Довгалюк Павло ІПЗ-41. Лаб 6. Варіант 5. Частина 2.\n")
 
-;==========селектори дійсної та уявної частин компл числа=====
-(define (Myreal-part z) (car z))
-(define (Myimag-part z) (cdr z))
-;============ тригонометрична форма компл числа======== ==
-(define (square x)
-(* x x))
-(define (magnitude1 z)
-(sqrt (+ (square (Myreal-part z)) (square (Myimag-part z)))))  
-(define (angle1 z)
-(atan (Myimag-part z) (Myreal-part z)))
-(define (make-from-real-imag x y)
-(cons x y))
-(define (make-from-mag-ang r a)
-(cons (* r (cos a)) (* r (sin a))))
+; Створити дві черги.
+; Об’єднати дві черги в одну,
+; елементи якої вибрані по черзі з вхідних черг, наприклад,
+; перший елемент першої черги, перший елемент другої,
+; другий елемент першої черги, другий елемент другої і т.д.. 
 
-;======= додавання та віднімання компл чисел в алгебраїчній формі======
-(define (add-complex z1 z2)
- (make-from-real-imag (+ (Myreal-part z1) (Myreal-part z2))
-    (+ (Myimag-part z1) (Myimag-part z2))))
 
-(define (sub-complex z1 z2)
-    (make-from-real-imag (- (Myreal-part z1) (Myreal-part z2))
-       (- (Myimag-part z1) (Myimag-part z2))))
+;;#######Implimenting Queue#########
+(define (make-queue)
+ (define p (cons '() '()))
+ (cons p p)
+)
+;========== ==Перевірка черги на пустоту==========
+(define (null-queue? q)
+ (and
+  (eq? (front q) (rear q)) (eq? (car (front q)) '() ))
+)
+;===== селектор (доступ) до першого елемента черги===
+(define (front q)
+ (car q)) 
+;==== селектор (доступ) до останнього елемента черги ==
+(define (rear q)
+ (cdr q))
+;========додавання нового елемента в чергу=========
+(define (push q e)
+ (define p (cons e '()))
+ (if (null-queue? q)
+  (begin (set-car! q p)
+   (set-cdr! q p)
+  )
+  (begin
+   (set-cdr! (rear q) p)
+   (set-cdr! q p)
+  ) ) )
+;========== вилучення елемента з черги============
+(define (pop q)
+ (define x 0)
+ (if (null-queue? q)
+  'Empty    
+  (if (and (eq? (front q) (rear q))  (eq? '() (cdr (front q)))   )
+   (begin
+    (set! x (car (front q)))
+    (set-car! (front q) '() )
+    x ) 
+   (begin
+    (set! x (car (front q)))
+    (set-car! q (cdr (front q)) )
+    x ))))
 
-(define complex1 (make-from-real-imag 2 3))      ;створення комплексного числа1
-(define complex2 (make-from-real-imag -5 1))
-;=====множення та ділення компл чисел в тригонометричній формі======
-(define (mul-complex z1 z2)
-        (make-from-mag-ang (* (magnitude1 z1) (magnitude1 z2))
-                           (+ (angle1 z1) (angle1 z2))))
-
-(define (div-complex z1 z2)
-        (make-from-mag-ang (/ (magnitude1 z1) (magnitude1 z2))
-                           (- (angle1 z1) (angle1 z2))))
-
-;=====список комплексних чисел======
-(define complex-list (list (make-from-real-imag 1 5)
-                           (make-from-real-imag 4 2)
-                           (make-from-real-imag 6 9)
-                           (make-from-real-imag 7 3)))
-
-;=====добуток комплексних чисел======
-(define (complex-list-sum complex-list)
-  (if (null? complex-list)
-      (make-from-real-imag 0 0)
-      (add-complex (car complex-list) (complex-list-sum (cdr complex-list))))
+;==========Об’єднання двух черг============
+(define (queue-join q1 q2)
+  (let* ((q (make-queue)))
+    (do ((index 0 (+ index 1))) ((and (null-queue? q1) (null-queue? q2)) q)
+      (if (not (null-queue? q1))
+          (push q (pop q1))
+          )
+      (if (not (null-queue? q2))
+          (push q (pop q2))
+          )
+      )
+    )
   )
 
-(display "Список комплексних чисел: ") complex-list  
-(display "Добуток комплексних чисел: ") (complex-list-sum complex-list)
+(define q1 (make-queue))
+(define q2 (make-queue))
+(push q1 1) (push q1 3) (push q1 5) (push q1 7) (push q1 9)
+(push q2 2) (push q2 4) (push q2 6) (push q2 8) 
+
+(display "Перша черга: ") q1
+(display "Друга черга: ") q2
+(display "Об'єднана черга: ") (queue-join q1 q2)
